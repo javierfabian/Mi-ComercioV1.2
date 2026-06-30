7c116402ed7a3d1c6548e1f40534ca680bbee7bc
javierfabian <javierfabian2107@hotmail.com>
2026-06-30 16:56:39 -0300

    colocando iconos definitivos

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'nexos-pos-v1';
-const APP_SHELL = ['./', './index.html', './manifest.json'];
+const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
